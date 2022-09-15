fc1d9aa8c621abaf4513123926f3dc1549a3d6ed
mnl000mnl <mnl000mnl@mail.ru>
2022-09-16 00:13:16 +0700

    added JavaDocs comments, tests, shell build script and edited code style

---
+++ Task_1_1_1/src/main/java/ru/nsu/amazyar/BinHeap.java
@@ -0,0 +1,127 @@
+package ru.nsu.amazyar;
+
+/**
+ * Executes heapSort
+ * <p>
+ * Sorts only int[]
+ * </p>
+ */
+public class BinHeap {
+  /**
+   * binary Heap, used in HeapSort algorithm
+   */
+  private int[] tree;
+  /**
+   * Number of elements in {@link #tree}
+   */
+  private int binLength = 0;
+
+  /**
+   * Initialises {@code #tree}
+   *
+   * @param arr initial array
+   */
+  private void createTree(int[] arr) {
+    int length = arr.length;
+    tree = new int[length];
+    this.binLength = length;
+    for (int i = 0; i < length; i++) {
+      tree[i] = arr[i];
+      siftUp(i);
+    }
+  }
+
+  /**
+   * @return number of elements in {@code #tree}
+   */
+
+  private int getLength() {
+    return this.binLength;
+  }
+
+  /**
+   * Returns an element from binary heap
+   *
+   * @param idx index of desired element in {@code #tree}
+   * @return element in idx position in {@code #tree}
+   */
+  private int getEl(int idx) {
+    return this.tree[idx];
+  }
+
+  /**
+   * Sets the element of binary heap
+   *
+   * @param idx      index of desired element in {@code #tree}
+   * @param newValue value to assign to desired element
+   */
+  private void setEl(int idx, int newValue) {
+    this.tree[idx] = newValue;
+  }
+
+  /**
+   * Changes values of two binary heap elements
+   *
+   * @param idxA index of the first element in {@code #tree}
+   * @param idxB index of the second element in {@code #tree}
+   */
+  private void swap(int idxA, int idxB) {
+    int temp = this.getEl(idxA);
+    this.setEl(idxA, this.getEl(idxB));
+    this.setEl(idxB, temp);
+  }
+
+  /**
+   * Ensures that a kid in HeapSort algorithm is smaller than its parent
+   *
+   * @param idx idx of a kid
+   */
+  private void siftUp(int idx) {
+    if (idx == 0) return;
+    int dadIdx = (idx + 1) / 2 - 1;
+    if (this.getEl(idx) < this.getEl(dadIdx)) {
+      this.swap(idx, dadIdx);
+      siftUp(dadIdx);
+    }
+  }
+
+  /**
+   * Ensures that parents in HeapSort are smaller than their children
+   *
+   * @param idx idx of a parent
+   */
+  private void siftDown(int idx) {
+    int smallest = idx;
+    int leftChild = 2 * idx + 1;
+    int rightChild = 2 * idx + 2;
+
+    if (leftChild < this.getLength() && this.getEl(leftChild) < this.getEl(smallest)) {
+      smallest = leftChild;
+    }
+
+    if (rightChild < this.getLength() && this.getEl(rightChild) < this.getEl(smallest)) {
+      smallest = rightChild;
+    }
+
+    if (smallest != idx) {
+      this.swap(smallest, idx);
+      siftDown(smallest);
+    }
+  }
+
+  /**
+   * Sorts an array using HeapSort
+   *
+   * @param arr initial array that becomes sorted after method execution
+   */
+  public void heapSort(int[] arr) {
+    this.createTree(arr);
+    int length = this.getLength();
+    for (int i = 0; i < length; i++) {
+      arr[i] = this.getEl(0);
+      this.swap(0, length - i - 1);
+      this.binLength--;
+      siftDown(0);
+    }
+  }
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/amazyar/HeapSortTest.java
@@ -0,0 +1,91 @@
+package ru.nsu.amazyar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HeapSortTest {
+    /**
+     * main binary heap that we will use
+     */
+    private BinHeap binHeap;
+
+    /**
+     * initialize binary heap
+     */
+    @BeforeEach
+    void beforeSortTest(){
+        binHeap = new BinHeap();
+    }
+
+    /**
+     * Passes an empty array
+     * Expects an empty array
+     */
+    @Test
+    void sortEmptyArray() {
+        int[] emptArr = {};
+        binHeap.heapSort(emptArr);
+        Assertions.assertArrayEquals(new int[0], emptArr);
+    }
+
+    /**
+     * Passes an array consisting of one element
+     * Expects an array consisting of the same element
+     */
+    @Test
+    void sortOneElem() {
+        int[] singleton = {5};
+        int[] hardRes = {5};
+        binHeap.heapSort(singleton);
+        Assertions.assertArrayEquals(hardRes, singleton);
+    }
+
+    /**
+     * Passes sorted array
+     * Expects the same array
+     */
+    @Test
+    void sortSorted() {
+        int[] sortArr = {-4,-3,-2,-1,0,1,2,3,4};
+        int[] hardRes = {-4,-3,-2,-1,0,1,2,3,4};
+        binHeap.heapSort(sortArr);
+        Assertions.assertArrayEquals(hardRes, sortArr);
+    }
+
+    /**
+     * Passes an array sorted in reversed order
+     * Expects the reversed array
+     */
+    @Test
+    void sortReversedSorted() {
+        int[] reSortArr = {4, 3, 2, 1, 0, -1, -2, -3, -4};
+        int[] hardRes = {-4,-3,-2,-1,0,1,2,3,4};
+        binHeap.heapSort(reSortArr);
+        Assertions.assertArrayEquals(hardRes, reSortArr);
+    }
+
+    /**
+     * Passes an array of the same element
+     * Expects the same array
+     */
+    @Test
+    void sortSameElem() {
+        int[] sameElemArr = {-3,-3,-3,-3,-3,-3,-3};
+        int[] hardRes = {-3,-3,-3,-3,-3,-3,-3};
+        binHeap.heapSort(sameElemArr);
+        Assertions.assertArrayEquals(sameElemArr, hardRes);
+    }
+
+    /**
+     * Passes an array of arbitrary numbers
+     * Expects a sorted array
+     */
+    @Test
+    void sortArbitraryTest(){
+        int[] justArr = {0, -2, 8, -10, 1, 2, 0, 2, -11, -10};
+        int[] hardRes = {-11, -10, -10, -2, 0, 0, 1, 2, 2, 8};
+        binHeap.heapSort(justArr);
+        Assertions.assertArrayEquals(justArr, hardRes);
+    }
+}
