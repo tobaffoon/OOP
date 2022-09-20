27240c1fb678c477b4b6c2de3011ae3063c70914
mnl000mnl <mnl000mnl@mail.ru>
2022-09-20 20:49:19 +0700

    deleted unnecessary Task_1_1_1

--- Task_1_1_1/shellBuild.bat
+++
@@ -1,3 +0,0 @@
-javadoc -d doc -charset utf-8  -sourcepath src\main\java -subpackages ru.nsu.amazyar
-javac -sourcepath .\src\main\java -d bin src\main\java\ru\nsu\amazyar\Main.java
-java -classpath ./bin ru.nsu.amazyar.Main
