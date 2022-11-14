f44449e5fa917bcaa7d26fc48c09eb06d1053e75
tobaffoon <tobaffoon@users.noreply.github.com>
2022-11-14 15:17:39 +0000

    Deploying to gh-pages from @ tobaffoon/OOP@8b88797faff48292575b64244c7bc78818b56de8 🚀

---
+++ Task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.amazyar","l":"SubstringFinder"},{"p":"ru.nsu.amazyar","l":"TwoCharBuffer"},{"p":"ru.nsu.amazyar","l":"ZFunctionCreator"}]
