19461e1ebf701536f2c748fa67d3eb6d65fe4aad
tobaffoon <a.mazyar@g.nsu.ru>
2022-11-26 22:49:01 +0700

    Start reusing getZfunction method instead of rewriting it. Deleted unnecessary variables

--- Task_1_3_1/src/main/java/ru/nsu/amazyar/SubstringFinder.java
+++ Task_1_3_1/src/main/java/ru/nsu/amazyar/SubstringFinder.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -56,61 +57,28 @@ public class SubstringFinder {
         int patternSize = pattern.length();
 
         TwoCharBuffer twoBuffer = new TwoCharBuffer(patternSize);   //buffer with two subbuffers
-        int bufferCap = reader.read(twoBuffer.getSecondBuffer());   //n of elements of last subbufer
+        int bufferCapacity = reader.read(twoBuffer.getSecondBuffer());   //n of elements of last subbufer
 
         //text is smaller than pattern => no need to check for entries
-        if (bufferCap < patternSize) {
+        if (bufferCapacity < patternSize) {
             return entries;
         }
-        int bufferSize = bufferCap;                                 //n of elements of twoBuffer
 
-        //boarders of the largest matched (with pattern) block of text from reader
-        int leftBoarder = 0;
-        int rightBoarder = 0;
+        int[] textZArray;
 
-        int effectiveIdx;                                           //index used for buffer
-        int currentEntryLength;                 //length of substring of text that matches pattern
+        //i is counter for buffers filled
+        for (int i = 0; ; i++) {
+            twoBuffer.switchBuffers();  //replace exhausted buffer with next one
+            bufferCapacity = reader.read(twoBuffer.getSecondBuffer());
 
-        //condition is false when buffered isn't fully filled (including empty buffer), and we've
-        //read all symbols from this half filled buffer
-        for (int i = 0; i % patternSize <= bufferCap; i++) {
-            effectiveIdx = i % patternSize;
-
-            //condition is true when first buffer is exhausted
-            if (effectiveIdx == 0) {
-                twoBuffer.switchBuffers();  //replace exhausted buffer with next one
-                bufferCap = reader.read(twoBuffer.getSecondBuffer());
-                if (bufferCap == -1) {
-                    bufferSize = patternSize;
-                } else {
-                    bufferSize = patternSize + bufferCap;
-                }
-            }
-
-            //doesn't equal to 0, when it's beginning substring already matched beginning of pattern
-            //here `rightBoarder - i` shows how far matching with pattern was already calculated
-            currentEntryLength = (rightBoarder > i)
-                ? Math.min(zpattern[i - leftBoarder], rightBoarder - i)
-                : 0;
-
-            //break if pattern is exhausted or buffer is exhausted (when it was not fully filled)
-            //or when symbols from pattern and buffer (text) don't match
-            while (currentEntryLength != patternSize
-                && effectiveIdx + currentEntryLength < bufferSize
-                && pattern.charAt(currentEntryLength)
-                    == twoBuffer.get(effectiveIdx + currentEntryLength)
-            ) {
-                currentEntryLength++;
-            }
+            textZArray = ZfunctionCreator.getZfunction(pattern + "\0" + Arrays.toString(
+                twoBuffer.getFirstBuffer()) + Arrays.toString(twoBuffer.getSecondBuffer()), patternSize + 1, zpattern);
 
             //if entry length matches length of the pattern then pattern is the entry
-            if (currentEntryLength == patternSize) {
-                entries.add(i);
-            }
-
-            if (i + currentEntryLength > rightBoarder) {
-                leftBoarder = i;
-                rightBoarder = i + currentEntryLength;
+            for (int entryLength : textZArray) {
+                if (entryLength == patternSize) {
+                    entries.add();
+                }
             }
         }
 
