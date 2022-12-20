3ec47a7215974efd45da4287d6650ecec36f56d1
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-20 10:18:53 +0700

    Created ConstantsFactory class to support E and PI

---
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/ConstantsFactory.java
@@ -0,0 +1,25 @@
+package ru.nsu.amazyar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConstantsFactory {
+    public static final Map<String, Double> CONSTANTS_FACTORY;
+
+    static {
+        CONSTANTS_FACTORY = new HashMap<>();
+
+        CONSTANTS_FACTORY.put("e", Math.E);
+        CONSTANTS_FACTORY.put("pi", Math.PI);
+    }
+
+    public static List<String> getAllowedConstants(){
+        return new ArrayList<>(CONSTANTS_FACTORY.keySet());
+    }
+
+    public static Double getConstant(String constant) {
+        return CONSTANTS_FACTORY.get(constant);
+    }
+}
