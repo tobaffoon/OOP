6b8f41310cc7630b1577a8d4f58e0ddde2cb8827
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-20 10:09:05 +0700

    Added operations to factory

--- Task_1_5_1/src/main/java/ru/nsu/amazyar/OperationFactory.java
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/OperationFactory.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import ru.nsu.amazyar.Operation;
 import ru.nsu.amazyar.Operations.Add;
+import ru.nsu.amazyar.Operations.Cos;
+import ru.nsu.amazyar.Operations.Divide;
+import ru.nsu.amazyar.Operations.Log;
+import ru.nsu.amazyar.Operations.Multiply;
+import ru.nsu.amazyar.Operations.Pow;
+import ru.nsu.amazyar.Operations.Sin;
+import ru.nsu.amazyar.Operations.Subtract;
 
 public class OperationFactory {
     public static final Map<String, Operation> OPERATION_FACTORY;
@@ -16,6 +20,13 @@ public class OperationFactory {
         OPERATION_FACTORY = new HashMap<>();
 
         OPERATION_FACTORY.put("+", new Add());
+        OPERATION_FACTORY.put("-", new Subtract());
+        OPERATION_FACTORY.put("*", new Multiply());
+        OPERATION_FACTORY.put("/", new Divide());
+        OPERATION_FACTORY.put("pow", new Pow());
+        OPERATION_FACTORY.put("log", new Log());
+        OPERATION_FACTORY.put("sin", new Sin());
+        OPERATION_FACTORY.put("cos", new Cos());
     }
 
     public static List<String> getAllowedOperations(){
