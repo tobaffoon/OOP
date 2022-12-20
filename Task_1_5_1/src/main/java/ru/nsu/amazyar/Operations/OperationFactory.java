2b8deb807a604d47cda84c3df67f68a1a5a53a32
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-20 09:39:42 +0700

    Revert "removed useless getAllowedOperations method"
    This reverts commit ec9194024104e32d7ce5b6b32cd4434c8571f39c.

--- Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/OperationFactory.java
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/OperationFactory.java
@@ -1,7 +1,11 @@
 package ru.nsu.amazyar.Operations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import ru.nsu.amazyar.Operation;
 
 public class OperationFactory {
@@ -12,6 +16,11 @@ public class OperationFactory {
 
         OPERATION_FACTORY.put("+", new Add());
     }
+
+    public static List<String> getAllowedOperations(){
+        return new ArrayList<>(OPERATION_FACTORY.keySet());
+    }
+
     public static Operation getOperation(String operator) {
         if (OPERATION_FACTORY.containsKey(operator)) {
             return OPERATION_FACTORY.get(operator);
