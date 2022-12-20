21693c61f90b39a4d0bb9174d980e110b74d422f
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-20 10:06:04 +0700

    Added all operations

---
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/Log.java
@@ -0,0 +1,20 @@
+package ru.nsu.amazyar.Operations;
+
+import java.util.List;
+import ru.nsu.amazyar.Operation;
+
+public class Log extends Operation {
+    @Override
+    public int getArity() {
+        return 1;
+    }
+
+    @Override
+    public double calculate(List<Double> arguments) {
+        if (arguments.size() != getArity()) {
+            throw new IllegalStateException("Wrong number of arguments");
+        }
+
+        return Math.log(arguments.get(0));
+    }
+}
---
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/Subtract.java
@@ -0,0 +1,20 @@
+package ru.nsu.amazyar.Operations;
+
+import java.util.List;
+import ru.nsu.amazyar.Operation;
+
+public class Subtract extends Operation {
+    @Override
+    public int getArity() {
+        return 2;
+    }
+
+    @Override
+    public double calculate(List<Double> arguments) {
+        if (arguments.size() != getArity()) {
+            throw new IllegalStateException("Wrong number of arguments");
+        }
+
+        return arguments.get(0) - arguments.get(1);
+    }
+}
