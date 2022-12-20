e589072d9504be2e04cb85d6676b29936b41ae56
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-20 09:38:21 +0700

    removed useless getAllowedOperations method

--- Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/OperationFactory.java
+++ Task_1_5_1/src/main/java/ru/nsu/amazyar/Operations/OperationFactory.java
@@ -1,11 +1,7 @@
 package ru.nsu.amazyar.Operations;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import ru.nsu.amazyar.Operation;
 
 public class OperationFactory {
@@ -16,11 +12,6 @@ public class OperationFactory {
 
         OPERATION_FACTORY.put("+", new Add());
     }
-
-    public static List<String> getAllowedOperations(){
-        return new ArrayList<>(OPERATION_FACTORY.keySet());
-    }
-
     public static Operation getOperation(String operator) {
         if (OPERATION_FACTORY.containsKey(operator)) {
             return OPERATION_FACTORY.get(operator);
