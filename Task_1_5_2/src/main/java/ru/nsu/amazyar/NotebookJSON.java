3aca26722d4aa3f8b0acd416bf67c4f7ac52c6d3
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-26 00:21:40 +0700

    Create read from JSON method

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
@@ -1,10 +1,11 @@
 package ru.nsu.amazyar;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class NotebookJSON {
     private static final String defaultFile = "notebook.json";
@@ -18,5 +19,14 @@ public class NotebookJSON {
     public static void writeNotebook(Notebook notebook) throws IOException {
         File outFile = new File(defaultFile);
         objectMapper.writeValue(outFile, notebook.getNotes());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void readNotebook(Notebook notebook) throws IOException{
+        File inFile = new File(defaultFile);
+        List<Note> notes = objectMapper.readValue(inFile, new TypeReference<List<Note>>(){});
+        for (Note note : notes) {
+            notebook.add(note);
+        }
     }
 }
