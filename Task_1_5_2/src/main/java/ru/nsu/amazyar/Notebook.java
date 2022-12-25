67cec770bdfc4e339ec8bdd3e237b2cd4ab486cc
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-25 22:33:22 +0700

    Define basic methods for Notebook

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/Notebook.java
@@ -1,5 +1,41 @@
 package ru.nsu.amazyar;
 
-public class Notebook {
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Notebook{
+    private final List<Note> notes;
+
+    public Notebook() {
+        notes = new LinkedList<>();
+    }
+
+    public void add(String name, String content){
+        if(this.contains(name) != null){
+            return;
+        }
+
+        Note newNote = new Note(name, content, LocalDateTime.now());
+        notes.add(newNote);
+    }
+
+    public boolean remove(String name){
+        Note existingNote = this.contains(name);
+        if(existingNote == null){
+            return false;
+        } else{
+            notes.remove(existingNote);
+            return true;
+        }
+    }
+
+    private Note contains(String name){
+        for (Note note : notes) {
+            if(note.getName().equals(name)){
+                return note;
+            }
+        }
+        return null;
+    }
 }
