266c061a26def0687c13f66a722fee9f9e48fe0d
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-25 22:40:05 +0700

    Define write method for NotebookJSON

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
@@ -1,5 +1,22 @@
 package ru.nsu.amazyar;
 
-public class NotebookJSON {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class NotebookJSON {
+    private static final String defaultFile = "notebook.json";
+
+    public static void writeNotebook(Notebook notebook) throws IOException{
+        writeNotebook(notebook, defaultFile);
+    }
+
+    public static void writeNotebook(Notebook notebook, String out) throws IOException {
+        try(FileWriter writer = new FileWriter(out)){
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(writer, notebook);
+        } catch (IOException e){
+            throw new IOException();
+        }
+    }
 }
