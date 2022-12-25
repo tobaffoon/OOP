18562a4cd635f652221d69f1c576bbd8c2aa7684
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-26 00:22:05 +0700

    Add simple test for read from JSON method

--- Task_1_5_2/src/test/java/ru/nsu/amazyar/NotebookTest.java
+++ Task_1_5_2/src/test/java/ru/nsu/amazyar/NotebookTest.java
@@ -1,19 +1,24 @@
 package ru.nsu.amazyar;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class NotebookTest {
     @Test
-    public void funnyTest() {
+    public void writeTest() {
         Notebook notebook = new Notebook();
         notebook.add("alphabet1", "abc");
         notebook.add("alphabet2", "def");
         notebook.add("alphabet3", "ghi");
         notebook.remove("alphabet2");
         Assertions.assertDoesNotThrow(() -> NotebookJSON.writeNotebook(notebook));
+    }
+
+    @Test
+    public void readTest() throws IOException {
+        Notebook notebook = new Notebook();
+        NotebookJSON.readNotebook(notebook);
+        Assertions.assertTrue(notebook.find("alphabet1") != null && notebook.find("alphabet3") != null);
     }
 }
