0d93c84dbb3e1e2d7b3bd0b136ad559afa63c39f
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-25 22:22:20 +0700

    Defined Note class

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/Note.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/Note.java
@@ -1,5 +1,27 @@
 package ru.nsu.amazyar;
 
-public class Note {
+import java.time.LocalDateTime;
 
+public class Note {
+    private final String name;
+    private final String content;
+    private final LocalDateTime creationTime;
+
+    public Note(String name, String content, LocalDateTime creationTime) {
+        this.name = name;
+        this.content = content;
+        this.creationTime = creationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
 }
