340e8d3a91ab26f1c0374a5bbf661d56fda47831
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-26 09:53:05 +0700

    Add toString method and date format field for Note

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/Note.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/Note.java
@@ -1,22 +1,34 @@
 package ru.nsu.amazyar;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Note {
     private String name;
     private String content;
     private LocalDateTime creationTime;
+    private final DateTimeFormatter localFormatter;
+    private final static DateTimeFormatter defaultFormatter = DateTimeFormatter.ofPattern(
+        "dd.MM.yyyy hh:mm",
+        Locale.ENGLISH);
 
     public Note(){
         name = "None";
         content = "None";
         creationTime = LocalDateTime.now();
+        localFormatter = defaultFormatter;
     }
 
     public Note(String name, String content, LocalDateTime creationTime) {
+        this(name, content, creationTime, defaultFormatter);
+    }
+
+    public Note(String name, String content, LocalDateTime creationTime, DateTimeFormatter formatter) {
         this.name = name;
         this.content = content;
         this.creationTime = creationTime;
+        this.localFormatter = formatter;
     }
 
     public boolean isBetween(LocalDateTime from, LocalDateTime to){
@@ -54,5 +66,11 @@ public class Note {
 
     public void setCreationTime(LocalDateTime creationTime) {
         this.creationTime = creationTime;
+    }
+
+    public String toString(){
+        return "Name: " + this.getName() + "\n"
+            + "Note: " + this.getContent() + "\n"
+            + "Added: " + this.getCreationTime().format(localFormatter) + "\n\n";
     }
 }
