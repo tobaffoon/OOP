a60003256aa568eb083776ab4e67d2d23ec9c62e
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-26 21:20:43 +0700

    Add javaDoc for Main

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/Main.java
@@ -4,17 +4,24 @@ import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
 
+/**
+ * Runs notebook program.
+ */
 public class Main {
 
+    /**
+     * Notebook program entry point.
+     */
     public static void main(String[] args) throws ParseException, IOException {
         CommandLine mainCommandLine = NotebookArgumentParser.parseArguments(args);
 
         Notebook notebook = new Notebook();
         NotebookJSON.readNotebook(notebook);
 
-        boolean notebookChanged = NotebookArgumentParser.executeCommandLine(mainCommandLine, notebook);
+        boolean notebookChanged =
+            NotebookArgumentParser.executeCommandLine(mainCommandLine, notebook);
 
-        if(notebookChanged) {
+        if (notebookChanged) {
             NotebookJSON.writeNotebook(notebook);
         }
     }
