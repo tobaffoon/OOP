ee96c3dd2e4d7e97fac5bb9226ddf9824ce6b21f
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-26 20:07:01 +0700

    Add javaDocs for NotebookJSON

--- Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
+++ Task_1_5_2/src/main/java/ru/nsu/amazyar/NotebookJSON.java
@@ -5,9 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 
+/**
+ * IO operations for Notebook using JSON.
+ */
 public class NotebookJSON {
+
     private static final String defaultFile = "notebook.json";
     private static final ObjectMapper objectMapper;
 
@@ -16,14 +22,28 @@ public class NotebookJSON {
         objectMapper.registerModule(new JavaTimeModule());
     }
 
+    /**
+     * Write Notebook as JSON to defaultFile.
+     *
+     * @param notebook written notebook
+     */
     public static void writeNotebook(Notebook notebook) throws IOException {
         File outFile = new File(defaultFile);
         objectMapper.writeValue(outFile, notebook.getNotes());
     }
 
-    public static void readNotebook(Notebook notebook) throws IOException{
+    /**
+     * Read Notebook from JSON defaultFile.
+     * <p>
+     * Method clears existing notes of notebook and replaces them with new ones
+     * </p>
+     *
+     * @param notebook notebook in which read occurs
+     */
+    public static void readNotebook(Notebook notebook) throws IOException {
         File inFile = new File(defaultFile);
-        List<Note> notes = objectMapper.readValue(inFile, new TypeReference<>(){});
+        List<Note> notes = objectMapper.readValue(inFile, new TypeReference<>() {
+        });
         for (Note note : notes) {
             notebook.add(note);
         }
