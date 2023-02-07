1e9a14691bb8978fd50017208f671c8d7f950fdd
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-07 14:15:21 +0700

    Make PrimeFinder interface with default sequential implementation

--- Task_2_1_1/src/main/java/ru/nsu/amazyar/Sequential_PrimeFinder.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.amazyar;
-
-import java.util.Collection;
-
-public class Sequential_PrimeFinder extends PrimeFinder{
-    public boolean containsNoPrimes(Collection<Integer> collection){
-        return false;
-    }
-}
