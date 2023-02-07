ad7df6695d62118ef46c4bc5d711e303839bfcfc
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-07 14:56:47 +0700

    Implemented multithreaded calculation

--- Task_2_1_1/src/main/java/ru/nsu/amazyar/Multithreading_PrimeFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/amazyar/Multithreading_PrimeFinder.java
@@ -1,9 +1,48 @@
 package ru.nsu.amazyar;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Multithreading_PrimeFinder implements PrimeFinder{
-    public static boolean containsNoPrimes(Collection<Integer> collection){
-        return false;
+
+    private static final int DEFAULT_THREADS_COUNT = 5;
+
+    private static class PrimeFinderThread extends Thread{
+        private final List<Integer> threadList;
+        private boolean primeFound = false;
+
+        public PrimeFinderThread(List<Integer> threadList) {
+            this.threadList = threadList;
+        }
+
+        @Override
+        public void run() {
+            this.primeFound = PrimeFinder.containsNoPrimes(this.threadList);
+        }
+
+        public boolean primeFound() {
+            return primeFound;
+        }
+    }
+    public static boolean containsNoPrimes(List<Integer> list, int threadsCount){
+        List<PrimeFinderThread> primeFinderThreads = new ArrayList<>(threadsCount);
+        int listSize = list.size();
+        for (int i = 0; i < threadsCount; i++) {
+            PrimeFinderThread pThread = new PrimeFinderThread(list.subList(i * listSize, (i+1) * listSize));
+            primeFinderThreads.add(pThread);
+            pThread.start();
+        }
+        for (PrimeFinderThread pThread : primeFinderThreads) {
+            try {
+                pThread.join();
+                if(pThread.primeFound()) return false;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return true;
+    }
+    public static boolean containsNoPrimes(List<Integer> list){
+        return containsNoPrimes(list, DEFAULT_THREADS_COUNT);
     }
 }
--- Task_2_1_1/src/main/java/ru/nsu/amazyar/ParallelStream_PrimeFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/amazyar/ParallelStream_PrimeFinder.java
@@ -1,9 +1,10 @@
 package ru.nsu.amazyar;
 
 import java.util.Collection;
+import java.util.List;
 
 public class ParallelStream_PrimeFinder implements PrimeFinder{
-    public static boolean containsNoPrimes(Collection<Integer> collection){
+    public static boolean containsNoPrimes(List<Integer> collection){
         return false;
     }
 }
