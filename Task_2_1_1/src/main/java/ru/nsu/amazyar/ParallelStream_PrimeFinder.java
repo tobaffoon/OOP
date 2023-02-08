0a6a924b1089f4fbad15a4c22dc58b86843b489b
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-08 21:54:49 +0700

    Implement ParallelStream_PrimeFinder

--- Task_2_1_1/src/main/java/ru/nsu/amazyar/ParallelStream_PrimeFinder.java
+++ Task_2_1_1/src/main/java/ru/nsu/amazyar/ParallelStream_PrimeFinder.java
@@ -1,10 +1,9 @@
 package ru.nsu.amazyar;
 
-import java.util.Collection;
 import java.util.List;
 
 public class ParallelStream_PrimeFinder extends PrimeFinder{
     public boolean containsNoPrimes(List<Integer> collection){
-        return false;
+        return collection.parallelStream().noneMatch(PrimeFinder::simplePrimeCheck);
     }
 }
