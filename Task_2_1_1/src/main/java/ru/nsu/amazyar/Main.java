749ab6191bd1b68c802bcef301cb6f8c6eb75318
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-08 20:56:40 +0700

    Add main class with io for UI

--- Task_2_1_1/src/main/java/ru/nsu/amazyar/Main.java
+++ Task_2_1_1/src/main/java/ru/nsu/amazyar/Main.java
@@ -1,5 +1,18 @@
 package ru.nsu.amazyar;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numbers = scanner.nextLine();
+        scanner.close();
+        List<Integer> array = Arrays.stream(numbers.split(" +"))
+            .map(Integer::parseInt).collect(Collectors.toList());
+        System.out.println(PrimeFinder.containsNoPrimes(array));
+    }
 }
