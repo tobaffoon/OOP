23db582b7a3e1e51a889c45d429f2f3a4866d467
tobaffoon <tobaffoon@users.noreply.github.com>
2023-02-14 08:59:22 +0000

    Deploying to gh-pages from @ tobaffoon/OOP@03d7c1feb95fa7637569cd0c51503c84a72c6b4b 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.amazyar","l":"BenchmarkRunner"},{"p":"ru.nsu.amazyar","l":"Main"},{"p":"ru.nsu.amazyar","l":"MultithreadingPrimeFinder"},{"p":"ru.nsu.amazyar","l":"ParallelStreamPrimeFinder"},{"p":"ru.nsu.amazyar","l":"PrimeFinder"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.amazyar","l":"BenchmarkRunner"},{"p":"ru.nsu.amazyar","l":"MultithreadingPrimeFinder"},{"p":"ru.nsu.amazyar","l":"ParallelStreamPrimeFinder"},{"p":"ru.nsu.amazyar","l":"PrimeFinder"}]
