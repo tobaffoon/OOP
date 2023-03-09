81263afba3aae14a93526f73d67324ac0f7fc17c
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-09 17:44:01 +0700

    Remove separate monitors for get and push methods

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/OrdersQueue.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/OrdersQueue.java
@@ -5,33 +5,31 @@ import java.util.Queue;
 
 public class OrdersQueue<T> {
     private final Queue<T> queue = new ArrayDeque<>(MAXSIZE);
-    private final Object EMPTY_QUEUE = new Object();
-    private final Object FULL_QUEUE = new Object();
     private static final int MAXSIZE = 150;
 
     public OrdersQueue() {
     }
 
     public T get() throws InterruptedException {
-        synchronized (EMPTY_QUEUE){
+        synchronized (queue){
             while(queue.isEmpty()){
-                EMPTY_QUEUE.wait();
+                queue.wait();
             }
 
             T topOrder = queue.poll();
-            FULL_QUEUE.notifyAll();
+            queue.notifyAll();
             return topOrder;
         }
     }
 
     public void push(T newOrder) throws InterruptedException {
-        synchronized (FULL_QUEUE){
+        synchronized (queue){
             while(queue.size() == MAXSIZE){
-                FULL_QUEUE.wait();
+                queue.wait();
             }
 
             queue.add(newOrder);
-            EMPTY_QUEUE.notifyAll();
+            queue.notifyAll();
         }
     }
 }
