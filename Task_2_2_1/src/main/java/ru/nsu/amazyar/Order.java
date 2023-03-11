93b5d99fadac3355a25cc17722bab06a3bf4ac96
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-11 13:24:52 +0700

    Add time to cook and deliver fields to Order class

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Order.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Order.java
@@ -13,8 +13,12 @@ public class Order {
     }
     private final UUID orderId;
     private final OrderState state;
+    private final long timeToCook;
+    private final long timeToDeliver;
 
-    public Order() {
+    public Order(long timeToCook, long timeToDeliver) {
+        this.timeToCook = timeToCook;
+        this.timeToDeliver = timeToDeliver;
         this.orderId = UUID.randomUUID();
         this.state = OrderState.ORDERED;
     }
@@ -25,5 +29,13 @@ public class Order {
 
     public OrderState getState() {
         return state;
+    }
+
+    public long getTimeToCook() {
+        return timeToCook;
+    }
+
+    public long getTimeToDeliver() {
+        return timeToDeliver;
     }
 }
