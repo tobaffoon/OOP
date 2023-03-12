51276eb623b8c3b7823b93a73321d2cb942adae1
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-12 15:25:18 +0700

    Separate queues for cooking and delivering (storage)

---
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Storage.java
@@ -0,0 +1,49 @@
+package ru.nsu.amazyar;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Storage<T> extends ArrayDeque<T>{
+    private final Queue<T> queue;
+    private final int capacity;
+
+    public Storage(int capacity) {
+        this.capacity = capacity;
+        this.queue = new ArrayDeque<>(capacity);
+    }
+
+    public T pop(){
+        synchronized (queue){
+            while(queue.isEmpty()){
+                try {
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            T topOrder = queue.poll();
+            queue.notifyAll();
+            return topOrder;
+        }
+    }
+
+    public void push(T newOrder) {
+        synchronized (queue) {
+            while(queue.size() == capacity){
+                try {
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            queue.add(newOrder);
+            queue.notifyAll();
+        }
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
