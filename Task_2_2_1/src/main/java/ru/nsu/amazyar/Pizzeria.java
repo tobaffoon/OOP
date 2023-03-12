6563424afb9834cbb038cd5f300eff4240d22970
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-12 23:01:15 +0700

    Add deliverymen trunk capacities initialisation

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Pizzeria.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Pizzeria.java
@@ -6,16 +6,20 @@ import java.util.List;
 import java.util.Queue;
 
 public class Pizzeria {
-    private final int chefsNumber;
-    private final int deliverymenNumber;
+    private final long chefsNumber;
+    private final long deliverymenNumber;
     private final Queue<Order> orderQueue = new ArrayDeque<>();
     private final Storage<Order> storage;
     private final List<Worker> chefs = new ArrayList<>();
     private final List<Worker> deliverymen = new ArrayList<>();
 
-    public Pizzeria(int chefsNumber, int deliverymenNumber, int storageCapacity) {
+    public Pizzeria(int chefsNumber, int storageCapacity, List<Long> deliverymenCapacities) {
         this.chefsNumber = chefsNumber;
-        this.deliverymenNumber = deliverymenNumber;
+        this.deliverymenNumber = deliverymenCapacities.size();
         this.storage = new Storage<>(storageCapacity);
+        for(Long capacity : deliverymenCapacities){
+            Deliveryman deliveryman = new Deliveryman(this, capacity);
+            deliverymen.add(deliveryman);
+        }
     }
 }
