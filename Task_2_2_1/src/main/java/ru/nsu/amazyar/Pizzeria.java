c6d16c346e0126b74e400369b9623731470b8c9c
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-12 15:25:42 +0700

    Add characteristics fields to Pizzeria class

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Pizzeria.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Pizzeria.java
@@ -1,5 +1,20 @@
 package ru.nsu.amazyar;
 
-public class Pizzeria {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
+public class Pizzeria {
+    private final int chefsNumber;
+    private final int deliverymenNumber;
+    private final Queue<Order> orderQueue = new ArrayDeque<>();
+    private final Storage<Order> storage;
+    private final List<Chef> chefs = new ArrayList<>();
+
+    public Pizzeria(int chefsNumber, int deliverymenNumber, int storageCapacity) {
+        this.chefsNumber = chefsNumber;
+        this.deliverymenNumber = deliverymenNumber;
+        this.storage = new Storage<>(storageCapacity);
+    }
 }
