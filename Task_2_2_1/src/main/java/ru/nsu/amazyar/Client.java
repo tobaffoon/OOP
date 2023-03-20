559b78fbd8e40953da09e279d6ad7194df115877
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-20 21:20:02 +0700

    Implement run method for Client

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Client.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Client.java
@@ -1,6 +1,11 @@
 package ru.nsu.amazyar;
 
+import java.util.Random;
+
 public class Client implements Runnable{
+    private static final int MAX_SLEEP_TIME = 30;
+    private static final int MAX_DELIVERY_TIME = 45;
+
     private final Pizzeria pizzeria;
 
     public Client(Pizzeria pizzeria) {
@@ -9,9 +14,17 @@ public class Client implements Runnable{
 
     @Override
     public void run() {
-        
+        Random randomTimeGenerator = new Random(Thread.currentThread().getId()); // randomness will depend on client's thread ID
+        while(true){
+            try {
+                Thread.sleep(randomTimeGenerator.nextInt(MAX_SLEEP_TIME));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            pizzeria.order(randomTimeGenerator.nextInt(MAX_DELIVERY_TIME));
+        }
     }
-
     public Pizzeria getPizzeria() {
         return pizzeria;
     }
