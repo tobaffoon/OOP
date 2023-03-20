2494c6f1f9173fc8a59d95de5ff4f933fd2bd021
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-20 22:05:21 +0700

    Implement Deliveryman run method

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Deliveryman.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Deliveryman.java
@@ -1,5 +1,11 @@
 package ru.nsu.amazyar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+
+import ru.nsu.amazyar.Order.OrderState;
+
 public class Deliveryman extends Worker{
     private static final long DEFAULT_TRUNK_CAPACITY = 10;
 
@@ -26,6 +32,30 @@ public class Deliveryman extends Worker{
 
     @Override
     public void run() {
+        List<Order> trunk = new ArrayList<>();
+        while(true){
+            if(pizzeria.readyPizzas() == 0){
+                trunk.add(pizzeria.takePizza());
+            }
+            else{
+                while(pizzeria.readyPizzas() != 0 && trunk.size() < trunkCapacity){
+                    trunk.add(pizzeria.takePizza());
+                }
+            }
 
+            for(Order order : trunk){
+                deliver(order);
+            }
+        }
+    }
+
+    public void deliver(Order order){
+        try {
+            Thread.sleep(order.getTimeToDeliver());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        order.setState(OrderState.DELIVERED);
     }
 }
