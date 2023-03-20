4b2e27ee0fcdd568c91dd1239bef0956a8e6d61e
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-20 22:39:22 +0700

    Add pizzeria setter to client

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/Client.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/Client.java
@@ -4,16 +4,25 @@ import java.util.Random;
 
 public class Client implements Runnable{
     private static final int MAX_SLEEP_TIME = 30;
-    private static final int MAX_DELIVERY_TIME = 45;
 
-    private final Pizzeria pizzeria;
+    private Pizzeria pizzeria = null;
+    private final int timeToDeliver;
 
-    public Client(Pizzeria pizzeria) {
+    public Client(Pizzeria pizzeria, int timeToDeliver) {
         this.pizzeria = pizzeria;
+        this.timeToDeliver = timeToDeliver;
+    }
+
+    public Client(int timeToDeliver) {
+        this.timeToDeliver = timeToDeliver;
     }
 
     @Override
     public void run() {
+        if(pizzeria == null){
+            throw new RuntimeException("Cannot execute clients without pizzeria");
+        }
+        
         Random randomTimeGenerator = new Random(Thread.currentThread().getId()); // randomness will depend on client's thread ID
         while(true){
             try {
@@ -22,10 +31,15 @@ public class Client implements Runnable{
                 e.printStackTrace();
             }
             
-            pizzeria.order(randomTimeGenerator.nextInt(MAX_DELIVERY_TIME));
+            pizzeria.order(timeToDeliver);
         }
     }
+
     public Pizzeria getPizzeria() {
         return pizzeria;
+    }
+
+    public void setPizzeria(Pizzeria pizzeria){
+        this.pizzeria = pizzeria;
     }
 }
