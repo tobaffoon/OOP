c23f4ccbb3281cb5b3054bbe884682bd0cea784b
tobaffoon <a.mazyar@g.nsu.ru>
2023-04-04 15:43:31 +0700

    Add naming functionality for client and workers threads

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/PizzeriaRunner.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/PizzeriaRunner.java
@@ -14,10 +14,17 @@ public class PizzeriaRunner implements Runnable{
             List<Client> clients = PizzeriaJsonReader.readClients(PizzeriaJsonReader.defaultClientsPath);
             for(Client client : clients){
                 client.setPizzeria(pizzeria);
-                new Thread(client);
             }
+            createAndRunThreads(clients, "Client");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void createAndRunThreads(List<? extends Runnable> runnables, String nameTemplate){
+        for(int i = 0; i < runnables.size(); i++){
+            Thread new_thread = new Thread(runnables.get(i), nameTemplate + "-" + i);
+            new_thread.start();
         }
     }
 }
