d2f5dd602ba3d7471f1c2ffb194f72928b744522
tobaffoon <a.mazyar@g.nsu.ru>
2023-04-04 17:16:40 +0700

    Add minimal sleep time for clients

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/pizzeria/Client.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/pizzeria/Client.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Client implements Runnable{
     private static final int MAX_SLEEP_TIME = 30000;
+    private static final int MIN_SLEEP_TIME = 5000;
 
     private Pizzeria pizzeria = null;
     private final int timeToDeliver;
@@ -26,7 +27,7 @@ public class Client implements Runnable{
         Random randomTimeGenerator = new Random(Thread.currentThread().getId()); // randomness will depend on client's thread ID
         while(true){
             try {
-                Thread.sleep(randomTimeGenerator.nextInt(MAX_SLEEP_TIME));
+                Thread.sleep(randomTimeGenerator.nextInt(MAX_SLEEP_TIME - MIN_SLEEP_TIME) + 5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
