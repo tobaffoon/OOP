e9300b83e035cc1599f2770476c0fea377737762
tobaffoon <a.mazyar@g.nsu.ru>
2023-04-05 00:46:23 +0700

    Add consistent sleep mode for Client

--- Task_2_2_1/src/main/java/ru/nsu/amazyar/pizzeria/Client.java
+++ Task_2_2_1/src/main/java/ru/nsu/amazyar/pizzeria/Client.java
@@ -13,6 +13,9 @@ public class Client implements Runnable {
     private Pizzeria pizzeria = null;
     private final int timeToDeliver;
 
+    private boolean consistentSleep = false;
+    private int consistentSleepTime;
+
     /**
      * Constructor with bound pizzeria.
      *
@@ -45,8 +48,12 @@ public class Client implements Runnable {
             Thread.currentThread().getId()); // randomness will depend on client's thread ID
         while (true) {
             try {
-                Thread.sleep(
-                    randomTimeGenerator.nextInt(MAX_SLEEP_TIME - MIN_SLEEP_TIME) + MIN_SLEEP_TIME);
+                if (consistentSleep) {
+                    Thread.sleep(consistentSleepTime);
+                } else {
+                    Thread.sleep(randomTimeGenerator.nextInt(MAX_SLEEP_TIME - MIN_SLEEP_TIME)
+                        + MIN_SLEEP_TIME);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -67,5 +74,19 @@ public class Client implements Runnable {
      */
     public void setPizzeria(Pizzeria pizzeria) {
         this.pizzeria = pizzeria;
+    }
+
+    /**
+     * Set mode in which client sleeps for constant time before making an order
+     *
+     * @param sleepTime time for client to sleep between orders
+     */
+    public void setConsistentSleep(int sleepTime) {
+        consistentSleep = true;
+
+        if (sleepTime < 0) {
+            sleepTime = 0;
+        }
+        consistentSleepTime = sleepTime;
     }
 }
